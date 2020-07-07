c1b9925e4d3c0061b388804763dfad180010af8b
Cristian Felipe Buitrago Rodriguez <36422189+buty619@users.noreply.github.com>
2020-07-07 00:06:05 -0500

    nnext js template

---
+++ pages/index.tsx
@@ -0,0 +1,51 @@
+// Components
+import Head from "next/head";
+import Layout from "../components/Layout";
+import { siteTitle } from "../components/Layout/Layout";
+import Link from "next/link";
+import fetch from "node-fetch";
+
+// Styles
+import utilStyles from "../styles/utils.module.scss";
+
+export const getStaticProps = async () => {
+	const preLoadData = await fetch(
+		"https://pokeapi.co/api/v2/pokemon-form/25"
+	).then((res) => res.json());
+	return { props: { preLoadData } };
+};
+
+const Home = ({ preLoadData }) => (
+	<Layout home>
+		<Head>
+			<title>{siteTitle}</title>
+		</Head>
+		<section className={utilStyles.headingMd}>
+			<h2>Template for make Next js app</h2>
+			<p>with:</p>
+			<ul>
+				<li>TypeScript</li>
+				<li>type Check</li>
+				<li>Global Types definition</li>
+				<li>Sass</li>
+				<li>classnames</li>
+				<li>set global css</li>
+				<li>set global utils css</li>
+				<li>use of getStaticProps</li>
+				<li>fetch to api</li>
+			</ul>
+			<h3>
+				<Link href="/posts/first-post">Go to example</Link>
+			</h3>
+			<section>
+				<h1 className={utilStyles.heading2Xl}>
+					Example of pre load Data from fetch API
+				</h1>
+				<p>{preLoadData.name}</p>
+				<img src={preLoadData.sprites.front_default} alt={preLoadData.name} />
+			</section>
+		</section>
+	</Layout>
+);
+
+export default Home;
---
+++ pages/posts/first-post/FirstPost.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+
+// Components
+import Head from "next/head";
+import Layout from "../../../components/Layout";
+import classnames from "classnames";
+
+// Styles
+import styles from "./FirstPost.module.scss";
+
+const firstPost = () => {
+	const [counter, setCounter] = React.useState(0);
+	const handleIncrease = () => {
+		setCounter(counter + 1);
+	};
+	return (
+		<Layout>
+			<Head>
+				<title>First Post</title>
+			</Head>
+			<h1 className={classnames(styles.title, styles.hover)}>
+				Example post to show to handle state and classnames
+			</h1>
+			<p>new state: {counter}</p>
+			<button onClick={handleIncrease}>increace</button>
+		</Layout>
+	);
+};
+export default firstPost;
